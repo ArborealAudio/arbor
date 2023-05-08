f6910b15dd5864bf984d246a023ce8a2ff15b0df
Arboreal Audio <arborealaudio@protonmail.com>
2023-05-08 13:41:41 -0400

    Add build & copy for Windows

--- build.sh
+++ build.sh
@@ -4,6 +4,8 @@ if [[ $(echo $OSTYPE) == "linux-gnu" ]]; then
   os="linux"
 elif [[ $(echo $OSTYPE) == "darwin" ]]; then
   os="macos"
+elif [[ $(echo $OSTYPE) == "msys" ]]; then # msys = bash on windows
+  os="windows"
 fi
 
 zig build
@@ -11,4 +13,6 @@ if [[ $os == "macos" ]]; then
   cp -pf zig-out/lib/libclap-raw.dylib ~/Library/Audio/Plug-Ins/CLAP/clap-raw.clap
 elif [[ $os == "linux" ]]; then
   cp -pf zig-out/lib/libclap-raw.so ~/.clap/clap-raw.clap
+elif [[ $os == "windows" ]]; then
+  cp -pf zig-out/lib/libclap-raw.dll ~/.clap/clap-raw.clap
 fi
