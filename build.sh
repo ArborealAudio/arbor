4c09129bac4f8ce169bd49bb800b3fd407d7c0d7
ArborealAudio <arborealaudio@protonmail.com>
2023-05-12 11:25:06 -0400

    Update build script

--- build.sh
+++ build.sh
@@ -1,17 +1,9 @@
 set -e
 
-if [[ $(echo $OSTYPE) == "linux-gnu" ]]; then
-  os="linux"
-elif [[ $(echo $OSTYPE) == "darwin" ]]; then
-  os="macos"
-elif [[ $(echo $OSTYPE) == "msys" ]]; then # msys = bash on windows
-  os="windows"
-fi
-
 zig build
-if [[ $os == "macos" ]]; then
+if [[ $(uname) == "Darwin" ]]; then
   cp -pf zig-out/lib/libclap-raw.dylib ~/Library/Audio/Plug-Ins/CLAP/clap-raw.clap
-elif [[ $os == "linux" ]]; then
+elif [[ $(uname) == "Linux" ]]; then
   cp -pf zig-out/lib/libclap-raw.so ~/.clap/clap-raw.clap
 elif [[ $os == "windows" ]]; then
   cp -pf zig-out/lib/libclap-raw.dll ~/.clap/clap-raw.clap
