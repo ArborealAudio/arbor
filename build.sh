3fcb3ecd303cf6b3ab1483bf1b9f94074429622a
ArborealAudio <arborealaudio@protonmail.com>
2023-05-12 11:28:51 -0400

    Update build script for windows Rebase atop prior commit

--- build.sh
+++ build.sh
@@ -5,6 +5,6 @@ if [[ $(uname) == "Darwin" ]]; then
   cp -pf zig-out/lib/libclap-raw.dylib ~/Library/Audio/Plug-Ins/CLAP/clap-raw.clap
 elif [[ $(uname) == "Linux" ]]; then
   cp -pf zig-out/lib/libclap-raw.so ~/.clap/clap-raw.clap
-elif [[ $os == "windows" ]]; then
+elif [[ $(echo $OSTYPE) == "msys" ]]; then
   cp -pf zig-out/lib/libclap-raw.dll ~/.clap/clap-raw.clap
 fi
