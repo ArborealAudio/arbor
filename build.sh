fb7cadc9aef08c625420da5a0ba2bc445d445fd1
Arboreal Audio <arborealaudio@protonmail.com>
2023-06-01 21:17:21 -0400

    Didn't need to echo that

--- build.sh
+++ build.sh
@@ -14,6 +14,6 @@ if [[ $(uname) == "Darwin" ]]; then
   cp -pf zig-out/lib/libclap-raw.dylib ~/Library/Audio/Plug-Ins/CLAP/clap-raw.clap
 elif [[ $(uname) == "Linux" ]]; then
   cp -pf zig-out/lib/libclap-raw.so ~/.clap/clap-raw.clap
-elif [[ $(echo $OSTYPE) == "msys" ]]; then
+elif [[ $OSTYPE == "msys" ]]; then
   cp -pf zig-out/lib/clap-raw.dll ~/.clap/clap-raw.clap
 fi
