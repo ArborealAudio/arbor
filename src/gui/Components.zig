8e57a2e012d38d104209d2d58ce6e0dc2b7b5984
Arboreal Audio <arborealaudio@protonmail.com>
2023-06-17 22:31:16 -0400

    Change to circle knob Add components module

---
+++ src/gui/Components.zig
@@ -0,0 +1,2 @@
+//! Components.zig
+//! Module for drawing GUI widgets
--- src/gui/Gui.zig
+++ src/gui/Gui.zig
@@ -6,37 +6,27 @@ const rl = @cImport({
     @cInclude("raylib.h");
 });
 const Plugin = @import("../Plugin.zig");
+const Components = @import("Components.zig");
 
-pub const GUI_WIDTH: c_uint = 800;
+pub const GUI_WIDTH: c_uint = 600;
 pub const GUI_HEIGHT: c_uint = 600;
 const centerX: f32 = @as(f32, GUI_WIDTH) / 2.0;
 const centerY: f32 = @as(f32, GUI_HEIGHT) / 2.0;
-
-pub const mix_slider = rl.Rectangle{
-    .x = centerX - 25.0,
-    .y = centerY - 50.0,
-    .width = 50.0,
-    .height = 100.0,
-};
 
 pub fn render(plugin: *Plugin) void {
     rl.BeginDrawing();
 
     rl.ClearBackground(.{
-        .r = 194,
-        .g = 194,
-        .b = 194,
+        .r = 24,
+        .g = 43,
+        .b = 51,
         .a = 255,
     });
 
     const reverb_amount = @floatCast(f32, plugin.params.values.mix);
+    _ = reverb_amount;
 
-    var d_mix_slider = mix_slider;
-    d_mix_slider.height = mix_slider.height * reverb_amount;
-    d_mix_slider.y = mix_slider.y + (mix_slider.height - d_mix_slider.height);
-
-    rl.DrawRectangleRec(mix_slider, rl.BLACK);
-    rl.DrawRectangleRec(d_mix_slider, rl.RED);
+    rl.DrawCircleLines(@floatToInt(c_int, centerX), @floatToInt(c_int, centerY), 100.0, rl.RAYWHITE);
 
     rl.EndDrawing();
 }
