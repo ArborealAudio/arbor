e6bf1095c4b138aebcf299eec016af3fff68421a
ArborealAudio <arborealaudio@protonmail.com>
2023-06-20 07:54:51 -0400

    Update display impl for Win32

--- src/gui/gui_w32.c
+++ src/gui/gui_w32.c
@@ -3,9 +3,11 @@
 #include <windowsx.h>
 #include <clap/clap.h>
 
-void implGuiSetParent(void *main, const clap_window_t *window) {
+void *implGuiCreateDisplay() {}
+
+void implGuiSetParent(void *display, void *main, const clap_window_t *window) {
 	SetParent((HWND)main, (HWND)window->win32);
 }
-void implGuiSetVisible(void *main, bool visible) {
+void implGuiSetVisible(void *display, void *main, bool visible) {
 	ShowWindow((HWND)main, visible ? SW_SHOW : SW_HIDE);
 }
