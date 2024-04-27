2e8293d990374e7a5a405a183cac4a8235c70179
Arboreal Audio <arborealaudio@protonmail.com>
2024-04-26 21:54:34 -0400

    Update build.zig to write proper MacOS bundle
    - Write formatted Info.plist and PkgInfo

--- build.zig
+++ build.zig
@@ -1,5 +1,4 @@
 const std = @import("std");
-const builtin = @import("builtin");
 const arbor = @import("src/arbor.zig");
 const Format = arbor.Format;
 
@@ -9,7 +8,6 @@ const Format = arbor.Format;
 pub fn build(b: *std.Build) !void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
-    // const os_tag = target.result.os.tag;
 
     const build_options = b.addOptions();
     const format = b.option(Format, "format", "Plugin format") orelse .CLAP;
@@ -31,6 +29,10 @@ pub fn build(b: *std.Build) !void {
         const plug = try buildExample(b, format, example, arbor_mod, target, optimize);
         b.installArtifact(plug);
         std.log.info("Built example: {s}\n", .{example});
+        const copy_cmd = try CopyStep.createStep(b, format, target, plug);
+        copy_cmd.step.dependOn(b.getInstallStep());
+        const copy_step = b.step("copy", "Copy plugin to system dir");
+        copy_step.dependOn(&copy_cmd.step);
     }
 
     // Creates a step for unit testing.
@@ -166,54 +168,142 @@ fn buildExample(
     return plug;
 }
 
-// ISSUE: Is the Zig build system broken? Is it just so much smarter than me that I can't
-// figure out how to copy a file? Not sure. Either way, the below does not work as a system
-// command bassed as a build action. Maybe someday.
-fn getPluginCopyCmd(
-    os: std.Target.Os.Tag,
+pub const CopyStep = struct {
+    const Step = std.Build.Step;
+    step: Step,
     format: Format,
-    output: *std.Build.Step.InstallArtifact,
-) []const []const u8 {
-    std.log.warn("Bro you are hard-coding your own home dir\n", .{});
+    target: std.Build.ResolvedTarget,
+    artifact: *Step.Compile,
+
+    pub fn createStep(
+        b: *std.Build,
+        format: Format,
+        target: std.Build.ResolvedTarget,
+        artifact: *Step.Compile,
+    ) !*CopyStep {
+        const self = try b.allocator.create(CopyStep);
+        self.* = .{
+            .step = Step.init(.{
+                .id = .custom,
+                .name = "Copy Step",
+                .owner = b,
+                .makeFn = make,
+            }),
+            .format = format,
+            .target = target,
+            .artifact = artifact,
+        };
+
+        return self;
+    }
+
+    pub fn make(step: *Step, _: *std.Progress.Node) !void {
+        const self: *CopyStep = @fieldParentPtr("step", step);
+        try pluginCopyStep(step.owner, self.target, self.format, self.artifact);
+    }
+};
+
+fn pluginCopyStep(
+    b: *std.Build,
+    target: std.Build.ResolvedTarget,
+    format: Format,
+    output: *std.Build.Step.Compile,
+) !void {
+    _ = b;
+    var arena = std.heap.ArenaAllocator.init(std.heap.c_allocator);
+    defer arena.deinit();
+    const allocator = arena.allocator();
+    const os = target.result.os.tag;
+    const home_dir = try std.process.getEnvVarOwned(allocator, "HOME");
     const install_dir = switch (format) {
         .CLAP => switch (os) {
-            .linux => "/home/alex/.clap/",
-            .macos => "/Users/alex/Library/Audio/Plug-Ins/CLAP/",
+            .linux => try std.mem.concat(allocator, u8, &.{ home_dir, "/.clap/" }),
+            .macos => try std.mem.concat(allocator, u8, &.{ home_dir, "/Library/Audio/Plug-Ins/CLAP/" }),
             .windows => "/Program Files/Common Files/CLAP/",
             else => @panic("Unsupported OS"),
         },
         .VST3 => switch (os) {
-            .linux => "/home/alex/.vst3/",
-            .macos => "/Users/alex/Library/Audio/Plug-Ins/VST3/",
+            .linux => try std.mem.concat(allocator, u8, &.{ home_dir, "/.vst3/" }),
+            .macos => try std.mem.concat(allocator, u8, &.{ home_dir, "/Library/Audio/Plug-Ins/VST3/" }),
             .windows => "/Program Files/Common Files/VST3/",
             else => @panic("Unsupported OS"),
         },
         .VST2 => switch (os) {
-            .linux => "/home/alex/.vst/",
-            .macos => "/Users/alex/Library/Audio/Plug-Ins/VST/",
+            .linux => try std.mem.concat(allocator, u8, &.{ home_dir, "/.vst/" }),
+            .macos => try std.mem.concat(allocator, u8, &.{ home_dir, "/Library/Audio/Plug-Ins/VST/" }),
             .windows => "/Program Files/Steinberg/VstPlugins/",
             else => @panic("Unsupported OS"),
         },
     };
 
-    // get generated file
-    const file = if (output.emitted_bin) |bin|
-        bin.generated.getPath()
-    else
-        @panic("No generated file\n");
+    var plugin_dir = try std.fs.openDirAbsolute(install_dir, .{});
+    defer plugin_dir.close();
+    // TODO: Don't hardcode MacOS shite, make a OS functions if yo uahve  to
+    const dest = try std.mem.concat(allocator, u8, &.{ output.name, ".clap/Contents" });
+    var contents_dir = try plugin_dir.makeOpenPath(dest, .{});
+    defer contents_dir.close();
+    const gen_file = output.getEmittedBin().generated.getPath();
+    // write copy bin file
+    _ = try std.fs.cwd().updateFile(
+        gen_file,
+        contents_dir,
+        try std.mem.concat(allocator, u8, &.{ "MacOS/", output.name }),
+        .{},
+    );
 
-    // get copy cmd
-    switch (os) {
-        .windows => {
-            switch (format) {
-                .CLAP => return &.{
-                    "copy",
-                    file,
-                    install_dir,
-                },
-                else => @panic("TODO"),
-            }
-        },
-        else => @panic("TODO"),
+    // write MacOS plist & bundle thing
+    // TODO: Actually format the plist
+    if (os == .macos) {
+        const plist = try contents_dir.createFile("Info.plist", .{});
+        defer plist.close();
+        try plist.writer().print(osx_bundle_plist, .{
+            output.name, //CFBundleExecutable
+            "com.ArborealAudio.Distortion", //CF BundleIdentifier
+            output.name, //CFBundleName
+            "0.1", //CFBundleShortVersion
+            "0.1", //CFBundleVersion
+            "(c) 2024 Arboreal Audio, LLC", //NSHumanReadableCopyright
+        });
+        const bndl = try contents_dir.createFile("PkgInfo", .{});
+        defer bndl.close();
+        try bndl.writeAll("BNDL????");
     }
 }
+
+const osx_bundle_plist =
+    \\<?xml version="1.0" encoding="UTF-8"?>
+    \\<!DOCTYPE plist PUBLIC "-//Apple Computer//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+    \\<plist version="1.0">
+    \\<dict>
+    \\	<key>CFBundleDevelopmentRegion</key>
+    \\	<string>English</string>
+    \\	<key>CFBundleExecutable</key>
+    \\	<string>{s}</string>
+    \\	<key>CFBundleGetInfoString</key>
+    \\	<string></string>
+    \\	<key>CFBundleIconFile</key>
+    \\	<string></string>
+    \\	<key>CFBundleIdentifier</key>
+    \\	<string>{s}</string>
+    \\	<key>CFBundleInfoDictionaryVersion</key>
+    \\	<string>6.0</string>
+    \\	<key>CFBundleName</key>
+    \\	<string>{s}</string>
+    \\	<key>CFBundlePackageType</key>
+    \\	<string>BNDL</string>
+    \\	<key>CFBundleShortVersionString</key>
+    \\	<string>{s}</string>
+    \\	<key>CFBundleSignature</key>
+    \\	<string>????</string>
+    \\	<key>CFBundleVersion</key>
+    \\	<string>{s}</string>
+    \\	<key>CSResourcesFileMapped</key>
+    \\	<true/>
+    \\	<key>NSHumanReadableCopyright</key>
+    \\	<string>{s}</string>
+    \\  <key>NSHighResolutionCapable</key>
+    \\  <true/>
+    \\</dict>
+    \\</plist>
+    \\
+;
