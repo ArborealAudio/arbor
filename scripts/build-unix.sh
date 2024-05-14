e9dd56adde4671df45bad59a171dfbcb491e85a9
Arboreal Audio <arborealaudio@protonmail.com>
2024-05-14 11:53:25 -0400

    Get rid of get_zig.sh

--- scripts/build-unix.sh
+++ scripts/build-unix.sh
@@ -2,7 +2,27 @@
 
 set -e
 
-source $PWD/scripts/get_zig.sh
+ZIG_VERSION="0.12.0"
+
+if [[ $(uname -s) == 'Darwin' ]]; then
+  OS="macos"
+elif [[ $(uname -s) == 'Linux' ]]; then
+  OS="linux"
+fi
+
+if [[ $(uname -m) == 'arm64' ]]; then
+  ARCH="aarch64"
+else
+  ARCH=$(uname -m)
+fi
+
+TRIPLE="${OS}-${ARCH}-${ZIG_VERSION}"
+
+echo "Getting Zig ${ZIG_VERSION} for ${ARCH} ${OS}"
+
+curl "https://ziglang.org/builds/zig-${TRIPLE}.tar.xz" | tar xJ
+
+ZIG="zig-${TRIPLE}/zig"
 
 echo "Zig path = ${ZIG}"
 
