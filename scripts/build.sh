52311c22db18bf0712a6197a5f813e7769c74da0
Arboreal Audio <arborealaudio@protonmail.com>
2024-05-16 12:24:16 -0400

    CI: I ain't too bright

--- scripts/build.sh
+++ scripts/build.sh
@@ -21,7 +21,7 @@ if [[ $OS == 'windows' ]]; then
 	# powershell -Command "Expand-Archive -Path ./zig-${TRIPLE}.zip -DestinationPath ."
 fi
 
-[ $OS != 'windows' ] && ZIG=zig.exe || ZIG="zig-${TRIPLE}/zig.exe"
+[ $OS != 'windows' ] && ZIG="zig-${TRIPLE}/zig" || ZIG="zig.exe"
 
 echo "Zig path = ${ZIG}"
 
