a5920e142d8ff11260a7857bf03668e6f3d0625d
abriccio <alexbriccio@gmail.com>
2025-10-10 09:26:57 -0400

    Build: Only use Zig linker on Linux

--- build.zig
+++ build.zig
@@ -71,7 +71,7 @@ pub fn addPlugin(b: *std.Build, config: BuildConfig, formats: []const Format) !v
             .optimize = config.optimize,
         }),
         .use_llvm = true,
-        .use_lld = false,
+        .use_lld = config.target.result.os.tag != .linux,
     });
 
     const arbor_mod = b.addModule("arbor", .{
@@ -188,7 +188,7 @@ fn buildPlugin(
             .strip = false,
         }),
         .use_llvm = true,
-        .use_lld = false,
+        .use_lld = build_config.target.result.os.tag != .linux,
     });
 
     return plug;
